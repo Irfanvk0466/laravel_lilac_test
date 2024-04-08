4a4e53ac1b84e86621696234781d5ed602c46faa
irfan <vkirfan6@gmail.com>
2024-04-08 14:29:11 -0600

    seeder change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class,
             DesignationsTableSeeder::class,
             DepartmentsTableSeeder::class,
+            UsersTableSeeder::class,
         ]);    }
 }
